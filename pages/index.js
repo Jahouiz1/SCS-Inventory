11926f866bae31a92ebc4f6f2724ea28759369d2
Jahouiz1 <chiajiaho@hotmail.com>
2026-01-16 02:25:28 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic'
 
-const App = dynamic(() => import('../app/page'), { ssr: false })
+const App = dynamic(() => import('../app/page.js'), { ssr: false })
 
 export default function Home() {
   return <App />
